7ceb1664e695a843c0ef7e066049cf6ad9243ffa
jacob <2627302231@qq.com>
2022-07-26 15:25:47 +0800

    udpate

---
+++ bin/uninstall-ali/uninstall-aliyundun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/bin/uninstall-aliyun
+. ./uninstall.sh
+. ./quartz_uninstall.sh
+
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+
+pkill -9 AliYunDun
+pkill -9 AliYunDunUpdate
+
+cd -
