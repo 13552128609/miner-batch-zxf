0d432b25bd535b4ac82abff3f47be59530a5ba90
jacob <2627302231@qq.com>
2022-07-26 15:37:21 +0800

    add clean

---
+++ clean.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+password=$(cat ./conf/pwd.txt)
+ips=($(cat ./conf/host.csv | awk -F',' {'print $5'}))
+pwds=($(cat ./conf/host.csv | awk -F',' {'print $10'}))
+validBeginIndex=0
+validEndIndex=${#ips[*]}
+userName=root
+
+if [ -n "$1" ]
+then
+        validBeginIndex=$1
+fi
+
+if [ -n "$2" ]
+then
+        validEndIndex=$2
+fi
+
+
+index=0
+for ip in ${ips[*]}
+do 
+	password=${pwds[index]}
+ if [ ${index} -eq 0 ]
+  then
+	((index++))
+	continue
+  fi
+
+  if [ ${index} -ge ${validBeginIndex} -a ${index} -le ${validEndIndex} ]
+  then
+  	echo ${index} ${ip}
+  	/usr/bin/sshpass -p ${password} ssh -o StrictHostKeyChecking=no root@${ip}  "sudo rm -rf ~/bin/*"
+  fi 
+  ((index++))
+done
